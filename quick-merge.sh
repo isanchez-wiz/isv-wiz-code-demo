c91d5b61f65f49fe61bc00aa0e98b944797225aa
isanvalwiz <ivan.sanchez@wiz.io>
2025-07-02 10:10:06 +0200

    Wiz test, simulation of a fix to trigger scanning process 250702-13924

--- quick-merge.sh
+++ quick-merge.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-R="${RANDOM}"
+R="$(date +%y%m%d)-${RANDOM}"
 BRANCH_NAME="fix-testing-${R}"
 CHANGE_TITLE="Fix simulation ${R}"
 CHANGE_DESCRIPTION="Wiz test, simulation of a fix to trigger scanning process ${R}"
