bf9c3990d3c85f10db98312695fc0b4a1efb9bfc
isanvalwiz <ivan.sanchez@wiz.io>
2025-07-02 10:05:46 +0200

    Wiz test, simulation of a fix to trigger scanning process 4934

---
+++ quick-merge.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+R="${RANDOM}"
+BRANCH_NAME="fix-testing-${R}"
+CHANGE_TITLE="Fix simulation ${R}"
+CHANGE_DESCRIPTION="Wiz test, simulation of a fix to trigger scanning process ${R}"
+# GET LATEST VERSION FROM MAIN BRANCH
+#git checkout main
+#git pull origin main
+# NEW LOCAL BRANCH FOR CHANGES
+git checkout -b ${BRANCH_NAME}
+# EDIT YOUR FILES
+git add .
+git commit -m "${CHANGE_DESCRIPTION}"
+git push origin ${BRANCH_NAME}
+gh pr create --base main --head ${BRANCH_NAME} --title "${CHANGE_TITLE}" --body "${CHANGE_DESCRIPTION}"
+gh pr merge ${BRANCH_NAME} --merge
+git branch -d ${BRANCH_NAME}
+git push origin --delete ${BRANCH_NAME}
